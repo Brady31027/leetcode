e8cf101528c758a2322c55c777ad2e52071a1ad5
Brady Cheng <brady31027@gmail.com>
2016-07-17 01:44:20 -0700

    leetcode prob 374 v3

--- 374.py
+++ 374.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def guessNumber(self, n):
+        if guess(n) == 0:
+            return n
         local_min, local_max = 1, n
-        guess_num = int((local_min + local_max)/2)
+        guess_num = int(round( (local_min + local_max)/2) )
         ret = guess(guess_num)
         while ret != 0:
             if ret == 1: # guess is lower
                 local_min = guess_num
             elif ret == -1: # guess is higher
                 local_max = guess_num
-            guess_num = int((local_min + local_max)/2)
+            guess_num = int(round( (local_min + local_max)/2) )
             ret = guess(guess_num)
         return guess_num
