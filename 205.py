81bc63d1f0496987b5e72a3d9c3d321b5d1e7118
Brady31027 <brady31027@gmail.com>
2016-07-25 17:09:39 -0700

    leetcode 205 v2

--- 205.py
+++ 205.py
@@ -5,6 +5,27 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        dict_s, set_t = {}, set()
+        for (src, tar) in zip(s, t):
+            if src in dict_s:
+                if dict_s[src] != tar:
+                    return False
+            else:
+                if tar in set_t:
+                    return False
+                dict_s[src] = tar
+                set_t.add(tar)
+            """
+            # following method is still failed the performance test
+            if dict_s.get(src) == None and dict_t.get(tar) == None:
+                dict_s[src] = tar
+                dict_t[tar] = src
+            elif dict_s.get(src) != tar or dict_t.get(tar) != src:
+                return False
+            """
+        return True
+        
+        """
         dict_pattern = {}
         pattern_len = len(s)
         for i in xrange(pattern_len):
@@ -17,3 +38,4 @@ class Solution(object):
             else:
                 dict_pattern[s[i]] = t[i]
         return True
+        """
