4dcb6646db65117dc5c02dc63ab6108079714e43
Brady31027 <brady31027@gmail.com>
2016-08-02 12:22:16 -0700

    leetcode 155 v2

--- 155.py
+++ 155.py
@@ -1,43 +1,45 @@
 class MinStack(object):
 
-    l_stack = []
+    l_stack, l_min_stack = [], []
+    
     def __init__(self):
         """
         initialize your data structure here.
         """
         self.l_stack = []
+        self.l_min_stack = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: void
         """
+        if len(self.l_min_stack) == 0 or x <= self.l_min_stack[-1]:
+            self.l_min_stack.append(x)
         self.l_stack.append(x)
-        
 
     def pop(self):
         """
         :rtype: void
         """
-        if len(self.l_stack) > 0:
-            self.l_stack.pop()
+        if self.top() == self.getMin():
+            self.l_min_stack.pop()
+        self.l_stack.pop()
         
     def top(self):
         """
         :rtype: int
         """
-        if len(self.l_stack) > 0:
-            return self.l_stack[ len(self.l_stack) - 1 ]
+        #better to have empty handling
+        return self.l_stack[-1]
         
 
     def getMin(self):
         """
         :rtype: int
         """
-        if len(self.l_stack) > 0:
-            l_sorted_stack = sorted(self.l_stack)
-            return l_sorted_stack[0]        
-
+        # better to have empty handling
+        return self.l_min_stack[-1]
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
