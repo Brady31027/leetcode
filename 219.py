bf73421e1d30e38c1fb22b5b11ed8e082d10d691
Brady Cheng <brady31027@gmail.com>
2016-08-01 22:44:12 -0700

    leetcode 219 v2 (performance tuning)

--- 219.py
+++ 219.py
@@ -5,14 +5,10 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        dict_book = {}
-        for i in range(len(nums)):
-            if nums[i] in dict_book.keys():
-                if i - dict_book.get(nums[i]) <= k:
+        dict_nums = {}
+        for i in range( len(nums) ):
+            if nums[i] in dict_nums:
+                if i - dict_nums[ nums[i] ] <= k:
                     return True
-                else:
-                    dict_book[nums[i]] = i
-            else:
-                dict_book[nums[i] ] = i
+            dict_nums[ nums[i] ] = i
         return False
-        
