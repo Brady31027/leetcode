b802e903eca685aa416d7b2d7de2023e59ba909c
Brady Cheng <brady31027@gmail.com>
2016-08-09 12:15:20 -0700

    leetcode 70 v2

--- 70.py
+++ 70.py
@@ -10,4 +10,9 @@ class Solution(object):
         # f(3) = 1+1+1 or 2+1 or 1+2 = f(2) + 1 or f(1) + 2 = f(1) + f(2)
         # f(n) = f(n-1) + f(n-2)a
         
-        return 1 if n < 2 else self.climbStairs(n-1) + self.climbStairs(n-2)
+        # recursive is too slow
+        # return 1 if n < 2 else self.climbStairs(n-1) + self.climbStairs(n-2)
+        l_fib = [1 for i in range(n+1)]
+        for i in range(2,n+1,1):
+            l_fib[i] = l_fib[i-1] + l_fib[i-2]
+        return l_fib[n]
