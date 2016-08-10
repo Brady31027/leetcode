4f4d65c4d3c49fe8cd77f684b170de8bceec1ff9
Brady Cheng <brady31027@gmail.com>
2016-08-10 14:56:32 -0700

    leetcode 204 v2

--- 204.py
+++ 204.py
@@ -1,20 +1,16 @@
-import math
-
 class Solution(object):
     def countPrimes(self, n):
         """
         :type n: int
         :rtype: int
         """
-        if n <= 2:
-            return 0
-        prime_cnt = 0
-        for i in range( 3, n, 2 ):
-            is_prime = True
-            for j in range(3, int(math.sqrt(i))+1, 2):
-                if i % j == 0:
-                    is_prime = False
-                    break
-            if is_prime == True:
-                prime_cnt += 1
-        return prime_cnt + 1
+        # [Sieve of Eratorsthenes]
+        # => if number a is mutiple of number b, then a is not a prime
+        # useful hints: xrange uses less memory than range
+        if n <= 2: return 0
+        l_is_prime = [1] * n
+        for i in xrange(2, n):
+            if l_is_prime[i]:
+                for j in xrange( i * 2, n, i):
+                    l_is_prime[j] = 0
+        return l_is_prime.count(1)-2 # 0 and 1 are not prime
