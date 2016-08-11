8f64ab6c0264a69d4d919b296e1adbfc0c525f35
Brady Cheng <brady31027@gmail.com>
2016-08-11 16:37:22 -0700

    leetcode 9 v3

--- 9.py
+++ 9.py
@@ -4,14 +4,7 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        # [version 2]
         while num > 0:
             reverse_num = (reverse_num * 10) + (num % 10)
             num /= 10
-        return True if reverse_num == x else False
-'''
-        # [version 1]
-        str_x = str(x)
-        reverse_str_x = str_x[::-1]
-        return True if str_x == reverse_str_x else False
-'''
+        return reverse_num == x
