1494805c20c826c52b0bfa7611297a277ee60c0d
Brady Cheng <brady31027@gmail.com>
2016-08-13 00:21:36 -0700

    leetcode 101 v2 (performance tuning)

--- 101.py
+++ 101.py
@@ -1,3 +1,4 @@
+tion for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
@@ -12,32 +13,11 @@ class Solution(object):
         """
         # BFS; swap queue
         if root is None: return True
-        #if root.left is None and root.right is None: return True
-        l_visit, l_children_idx = [root], []
+        l_visit = [root]
         while l_visit:
-            l_visit2, l_children_idx2 = [], []
-            for idx in range(len(l_visit)):
-                node = l_visit[idx]
-                if node.left: 
-                    l_visit2.append(node.left)
-                    l_children_idx2.append(1)
-                else:
-                    l_children_idx2.append(0)
-                if node.right: 
-                    l_visit2.append(node.right)
-                    l_children_idx2.append(1)
-                else:
-                    l_children_idx2.append(0)
-                    
-            l_visit = l_visit2
-            l_children_idx = l_children_idx2
-            
-            if l_children_idx != l_children_idx[::-1]:
-                return False
-            
-            if [n.val for n in l_visit] != [n.val for n in l_visit][::-1]: 
-                return False
-            
-            
+            l_children = []
+            for n in l_visit: l_children.extend([n.left, n.right])
+            l_children_val = [ (node.val if node else None) for node in l_children]
+            if l_children_val != l_children_val[::-1]: return False
+            l_visit = [node for node in l_children if node]
         return True
-        
