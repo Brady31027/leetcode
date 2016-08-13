88da4d13c1453cf0e4375526a45a33c340278a7c
Brady Cheng <brady31027@gmail.com>
2016-08-12 23:12:52 -0700

    leetcode 88 v2

--- 88.py
+++ 88.py
@@ -1,4 +1,4 @@
-class Solution(object):
+ass Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
         :type nums1: List[int]
@@ -7,6 +7,23 @@ class Solution(object):
         :type n: int
         :rtype: void Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(n):
-            nums1[m + i] = nums2[i]
+        #[version 2]
+        while n > 0:
+            if m <= 0 or nums1[m - 1] < nums2[n - 1]:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+            else:
+                nums1[m + n - 1] = nums1[m - 1]
+                #nums1[m - 1] = nums2[n - 1] # no need, will be overwirte later
+                m -= 1
+                
+        # [version 2]
+        """
+        nums1[m:n+m] = nums2[:n]
         nums1.sort()
+        """
+        # [version 1]
+        """
+        for i in range(n): nums1[m + i] = nums2[i]
+        nums1.sort()
+        """
