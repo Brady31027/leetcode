8403490dfedd95ba6a3d5b5090ca4050a98c771b
Brady Cheng <brady31027@gmail.com>
2016-08-13 17:11:41 -0700

    leetcode 112 v2

--- 112.py
+++ 112.py
@@ -5,39 +5,19 @@
 #         self.right = None
 
 class Solution(object):
-    
-    total = 0
-    result = False
-    
-    def hasPathSum(self, root, sum):
+      def hasPathSum(self, root, sum):
         """
         :type root: TreeNode
         :type sum: int
         :rtype: bool
         """
-        #total = 0
-        set_visited = set()
-        
-        if root is None:
-            return False
-        
-        # dfs is better
-        def dfs(node):
-            if self.result == True: return
-            self.total += node.val
-            if node.left and node.left not in set_visited:
-                set_visited.add(node.left)
-                dfs(node.left)
-            if node.right and node.right not in set_visited:
-                set_visited.add(node.right)
-                dfs(node.right)
-            if node.left is None and node.right is None:
-                #print self.total
-                if self.total == sum: self.result = True
-                else: self.total -= node.val
-            else:
-                self.total -= node.val
-                return
-        dfs(root)
-        return self.result
-        
+        if root is None: return False
+        if not root.left and not root.right: return sum == root.val
+        #if root.left:
+        #    left = self.hasPathSum(root.left, sum - root.val)
+        #    if left: return True
+        #if root.right:
+        #    right = self.hasPathSum(root.right, sum - root.val)
+        #    if right: return True
+        #return False
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
