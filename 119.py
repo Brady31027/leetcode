eb8e1dc434af4919de7a56b618d0d8f745947ff6
Brady Cheng <brady31027@gmail.com>
2016-08-13 17:48:15 -0700

    leetcode 119 v2

--- 119.py
+++ 119.py
@@ -4,22 +4,8 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        dict_row = {0: [1],
-                    1: [1, 1],
-                    2: [1, 2, 1],
-                    3: [1, 3, 3, 1]}
-                    
-        #expand more, performance boosts more
-        if rowIndex < 4:
-            return dict_row[rowIndex]
-        else:
-            l_prev_row, l_row = dict_row[3], [0]*(rowIndex + 1)
-            for i in range(4, rowIndex + 1, 1):
-                midIndex = i/2 # 0 base to 1 base 
-                for j in range(0, midIndex + 1 , 1):
-                    if j == 0:
-                        l_row[j] = l_row[i - j] = 1
-                    else:
-                        l_row[j] = l_row[i - j] = l_prev_row[j-1] + l_prev_row[j]
-                l_prev_row = list(l_row)
-            return l_row
+        # Hint: Any row can be constructed using the offset sum of the previous row
+        row = [1]
+        for i in range(rowIndex):
+            row = [u + v for u, v in zip(row+[0], [0]+row)]
+        return row
