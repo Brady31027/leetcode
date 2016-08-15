497782fa930e1771b645b50b9e71248d869a6322
Brady Cheng <brady31027@gmail.com>
2016-08-15 00:10:43 -0700

    leetcode 189 v2

--- 189.py
+++ 189.py
@@ -1,4 +1,18 @@
 class Solution(object):
+    
+    """
+    input : 1, 2, 3, 4, 5 (k = 3)
+    output: 3, 4, 5, 1, 2
+    algorithm:
+      (1) reverse second half: 1, 2, 5, 4, 3
+      (2) reverse first half : 2, 1, 5, 4, 3
+      (3) reverse all: 3, 4, 5, 1, 2
+    """
+    def rev(self, l_n, head, tail):
+            while head < tail:
+                l_n[head], l_n[tail] = l_n[tail], l_n[head]
+                head, tail = head + 1, tail - 1
+        
     def rotate(self, nums, k):
         """
         :type nums: List[int]
@@ -6,9 +20,8 @@ class Solution(object):
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         size = len(nums)
-        if k == 0 or size == 0:
-            return
-        tmp = list(nums)
-        for i in range(size):
-            nums[ (i+k) % size] = tmp[i] 
-        
+        if k == 0: return
+        k %= size # prevent circular shift
+        self.rev(nums, size-k, size-1)
+        self.rev(nums, 0, size-k-1)
+        self.rev(nums, 0, size-1)
