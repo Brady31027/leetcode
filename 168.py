760c7d104e22e644a080bdf073779f80cfbbd73d
Brady31027 <brady31027@gmail.com>
2016-08-14 17:10:35 -0700

    leetcode 168 v3

--- 168.py
+++ 168.py
@@ -1,12 +1,21 @@
+"""
+https://discuss.leetcode.com/topic/6245/python-solution-with-explanation
+https://discuss.leetcode.com/topic/47463/simple-python-solution
+"""
+
 class Solution(object):
+    
+    # move table to global is more efficient
+    capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
+        
     def convertToTitle(self, n):
         """
         :type n: int
         :rtype: str
         """
-        capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
         result = ''
         while n > 0:
-            result = capitals[(n-1)%26] + result
-            n = (n-1) // 26
+            # use divmod is faster
+            n, r = divmod(n-1, 26)
+            result = self.capitals[r] + result 
         return result
