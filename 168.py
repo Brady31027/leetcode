887a4884a0ceda6ec297a327f2870934d588736d
Brady31027 <brady31027@gmail.com>
2016-08-14 17:03:32 -0700

    leetcode 168 v2 (performance tuning)

--- 168.py
+++ 168.py
@@ -4,8 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
+        capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
         result = ''
         while n > 0:
-            result = chr( (n-1) % 26 + ord('A')) + result
-            n = (n-1) / 26
+            result = capitals[(n-1)%26] + result
+            n = (n-1) // 26
         return result
