40fabddc7d139c9473db5ef5eec58bac170a3459
Brady31027 <brady31027@gmail.com>
2016-08-15 12:40:49 -0700

    leetcode 20 v2

--- 20.py
+++ 20.py
@@ -1,13 +1,24 @@
-class Solution(object):
+ss Solution(object):
+    
     def isValid(self, s):
+        # can not handle other char or nested parentheses
+        size = len(s)
+        if size == 0: return True
+        if size % 2 != 0: return False
+        while '{}' in s or '[]' in s or '()' in s:
+            s = s.replace('{}','').replace('[]','').replace('()','')
+        return len(s) == 0
+        
         """
-        :type s: str
-        :rtype: bool
-        """
-        if len(s) == 0: return True
+        # [following is the regular solution]
         dict_syntax = {'{':'}','[':']','(':')'}
         l_head = dict_syntax.keys()
         l_tail = dict_syntax.values()
+   
+   
+        if len(s) == 0: return True
+        if len(s) % 2 != 0: return False
+        
         l_stack = []
         for c in s:
             if c in l_head: l_stack.append(c)
@@ -17,3 +28,4 @@ class Solution(object):
                 if c != dict_syntax[head]: return False
         if len(l_stack) > 0: return False
         return True
+        """
