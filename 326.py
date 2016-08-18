5b8ad66b435f8bd12ba50c21af5a8197ce34a627
Brady Cheng <brady31027@gmail.com>
2016-08-17 23:07:20 -0700

    leetcode 326 v2

--- 326.py
+++ 326.py
@@ -4,9 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n > 0 and (3**round(math.log(n, 3)) == n):
-            return True
-        else:
-            return False
-
+        return n > 0 and 1162261467 % n == 0
+        #return True if n > 0 and (math.log10(n)/math.log10(3)).is_integer() else False
+        #return True if n > 0 and if n > 0 and (3**round(math.log(n, 3)) == n) else False
         
