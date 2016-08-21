2cecd4461258d8f71cb8c2b1980833fd55ba53ae
Brady31027 <brady31027@gmail.com>
2016-08-20 17:11:43 -0700

    leetcode 39 v1 (performance tuning)

--- 39.py
+++ 39.py
@@ -1,18 +1,27 @@
-
-from itertools import combinations
-
 class Solution(object):
+    
+    ll_result = []
+    
     def combinationSum(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-        l_ans = []
-        l_filtered_input = sorted([x for x in candidates if x <= target])
-        for i in range(len(l_filtered_input)):
-            for comb in combinations(l_filtered_input, i):
-                total = sum(comb)
-                if total > target: break
-                elif total == target: l_ans.append(list(comb))
-        return l_ans
+        if candidates is None: return []
+        self.ll_result = []
+        
+        def dfs(candidates, target, comb):
+            total = sum(comb)
+            if total > target: return
+            elif total == target:
+                self.ll_result.append(comb)
+                return
+            else:
+                for i, v in enumerate(candidates):
+                    dfs(candidates[i:], target, comb + [v])
+                    
+        candidates.sort()
+        dfs(candidates, target, [])
+        return self.ll_result
+                    
