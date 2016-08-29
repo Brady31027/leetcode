2f24ca51f9949402c18b9a01601dc91adf1dea77
Brady Cheng <brady31027@gmail.com>
2016-08-28 22:08:25 -0700

    leetcode 167 v2 (binary tr tree seems better)

--- 167.py
+++ 167.py
@@ -5,9 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] + nums[j] > target: break
-                elif i == j: continue
-                elif nums[i] + nums[j] == target: return [min(i+1,j+1), max(i+1,j+1)]
-        return []
+        left, right = 0, len(nums)-1
+        while nums[left] + nums[right] != target:
+            if nums[left] + nums[right] < target: left += 1
+            else: right -= 1
+        return [left+1, right+1]
