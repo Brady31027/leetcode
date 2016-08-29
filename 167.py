e8f3f3ba7c6575c5bad9d71d4256872803aeb0c7
Brady Cheng <brady31027@gmail.com>
2016-08-28 22:31:27 -0700

    leetcode 167 v3

--- 167.py
+++ 167.py
@@ -7,6 +7,6 @@ class Solution(object):
         """
         left, right = 0, len(nums)-1
         while nums[left] + nums[right] != target:
-            if nums[left] + nums[right] < target: left += 1
-            else: right -= 1
+            if nums[left] + nums[right] > target: right -= 1
+            else: left += 1
         return [left+1, right+1]
