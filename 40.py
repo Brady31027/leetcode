28d8893e24d2d2e095242e7bb0e53ca914e01a40
Brady Cheng <brady31027@gmail.com>
2016-08-30 16:15:50 -0700

    leetcode 40 v3

--- 40.py
+++ 40.py
@@ -2,10 +2,13 @@ class Solution(object):
     ll_ans = []
     def dfs(self, candidates, target, start, l_ans):
         if target == 0 and l_ans not in self.ll_ans: return self.ll_ans.append(l_ans)
-        for i in range(start, len(candidates)): 
+        for i in xrange(start, len(candidates)):
+            if i != start and candidates[i] == candidates[i-1]: continue # ?? why ??
             if candidates[i] > target: return
             self.dfs(candidates, target - candidates[i], i + 1, l_ans + [candidates[i]])
+            
     def combinationSum2(self, candidates, target):
+        if target == 0: return [[]]
         self.ll_ans = []
         candidates.sort()
         self.dfs(candidates, target, 0, [])
