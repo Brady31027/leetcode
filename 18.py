b8dff639fdf6964bcf5b8860c4157f76042709a5
Brady Cheng <brady31027@gmail.com>
2016-08-30 17:23:08 -0700

    leetcode 18 v2

--- 18.py
+++ 18.py
@@ -1,16 +1,25 @@
-
-from itertools import combinations
-
 class Solution(object):
     def fourSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        ll_result = []
-        ll_candidates = [sorted(list(comb)) for comb in combinations(nums, 4) if sum(comb) == target ]
-        for l_c in ll_candidates:
-            if l_c in ll_result: continue
-            else: ll_result.append(l_c)
-        return ll_result
+        def findNsum(nums, target, N, result, results):
+            if len(nums) < N or N < 2 or target < nums[0] * N or target > nums[-1] * N: return
+            if N == 2: # two pointers solve sorted 2-sum problem
+                left,right = 0, len(nums)-1
+                while left < right:
+                    s = nums[left] + nums[right]
+                    if s == target:
+                        results.append(result + [nums[left], nums[right]])
+                        left += 1
+                        while left < right and nums[left] == nums[left-1]: left += 1
+                    elif s < target:
+                        left += 1
+                    else:
+                        right -= 1
+            else: # recursively reduce N
+                for i in xrange(len(nums) - N + 1):
+                    if i == 0 or (i > 0 and nums[i-1] != nums[i]):
+                        findNsum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
+
+        results = []
+        nums.sort()
+        findNsum(nums, target, 4, [], results)
+        return results
