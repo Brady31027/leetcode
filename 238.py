95a00b32833ddf79e93457f4cd29f6f5f573c5d4
Brady Cheng <brady31027@gmail.com>
2016-09-01 15:36:22 -0700

    leetcode 238 v2 (performance failed)

--- 238.py
+++ 238.py
@@ -4,13 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums2 = [0]*len(nums)
+        product,zero_cnt = 1, 0
+        for n in nums:
+            if n == 0:
+                zero_cnt +=1
+                if zero_cnt > 1: break
+                continue
+            product *= n
+            
         for i in range(len(nums)):
-            product = 1
-            for j in range(len(nums)):
-                if i == j: continue
-                else:
-                    product *= nums[j]
-                if product == 0: break
-            nums2[i] = product
-        return nums2
+            if zero_cnt > 1: return [0]*len(nums)
+            else:
+                if nums[i] == 0: nums[i] = product
+                elif zero_cnt > 0: nums[i] = 0
+                else: nums[i] = product // nums[i]
+        return nums
