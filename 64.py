7f99ae4af5c672a5df2e3af90ebc8ecf73069986
Brady Cheng <brady31027@gmail.com>
2016-10-23 22:37:02 -0700

    leetcode 64 v2

--- 64.py
+++ 64.py
@@ -4,21 +4,15 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        min_sum = 9999
         dim_y, dim_x = len(grid), len(grid[0])
-        l_sum, l_to_be_traversed = [], [[0,0,grid[0][0]]]
-        while l_to_be_traversed:
-            node = l_to_be_traversed.pop(0)
-            cur_x, cur_y, cur_sum = node[1], node[0], node[2]
-            if cur_x == dim_x - 1 and cur_y == dim_y - 1:
-                min_sum = min(cur_sum, min_sum)
-                continue
-            if (cur_x < dim_x - 1) and (cur_y < dim_y - 1):
-                l_to_be_traversed.append([cur_y + 1, cur_x, cur_sum + grid[cur_y + 1][cur_x]])
-                c = grid[cur_y][cur_x + 1]
-                l_to_be_traversed.append([cur_y, cur_x + 1, cur_sum + grid[cur_y][cur_x + 1]])
-            elif cur_x < dim_x - 1:
-                l_to_be_traversed.append([cur_y, cur_x + 1, cur_sum + grid[cur_y][cur_x + 1]])
-            elif cur_y < dim_y - 1:
-                l_to_be_traversed.append([cur_y + 1, cur_x, cur_sum + grid[cur_y + 1][cur_x]])
-        return min_sum
+        dp = [[0 for i in xrange(dim_x)] for j in xrange(dim_y)]
+        dp[0][0] = grid[0][0]
+        # for x axis
+        for i in xrange(1, dim_x): dp[0][i] = dp[0][i-1] + grid[0][i]  
+        # for y axis
+        for j in xrange(1, dim_y): dp[j][0] = dp[j-1][0] + grid[j][0]
+        # start dp
+        for y in xrange(1, dim_y):
+            for x in xrange(1, dim_x):
+                dp[y][x] = min(dp[y-1][x], dp[y][x-1]) + grid[y][x]
+        return dp[dim_y - 1][dim_x - 1]
