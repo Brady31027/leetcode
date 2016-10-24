0ca4048ff4923385cfb6371e4e816ce48f20732f
Brady Cheng <brady31027@gmail.com>
2016-10-23 23:24:11 -0700

    leetcode 318 v3

--- 318.py
+++ 318.py
@@ -4,16 +4,14 @@ class Solution(object):
         :type words: List[str]
         :rtype: int
         """
-        max_prod, l_nums = 0, []
+        max_prod, dict_nums = 0, {}
         for w in words:
             w_sum = 0
-            for c in set(w): w_sum |= 1 << (ord(c) - ord('a'))
-            l_nums.append(w_sum)
-        size = len(words)    
-        for w_i in xrange(size):
-            for w_j in xrange(size):
-                if l_nums[w_i] & l_nums[w_j] == 0: 
-                    prod = len(words[w_j]) * len(words[w_i])
+            for c in set(w): w_sum |= 1 << (ord(c) - 97)
+            dict_nums[w_sum] = max(dict_nums.get(w_sum, 0), len(w))
+        for x in dict_nums:
+            for y in dict_nums:
+                if not x & y:
+                    prod = dict_nums[x] * dict_nums[y]
                     if prod > max_prod: max_prod = prod
         return max_prod
-                
