259a40c849d607c00bf8c8718b5f9a659575d960
Brady Cheng <brady31027@gmail.com>
2016-10-24 14:31:51 -0700

    leetcode 400 v2 (performance failedd)

--- 400.py
+++ 400.py
@@ -5,6 +5,9 @@ class Solution(object):
         :rtype: int
         """
         l_long_str = ''
-        for i in xrange(n+1):
+        counter = 0
+        for i in xrange(1, n+1):
             l_long_str += str(i)
-        return int(l_long_str[i])
+            counter += int(math.log10(i)) + 1
+            if counter >= n: break
+        return int(l_long_str[n-1])
