bd0f0b8f9708ebd4979f831ce82d9da959890786
Brady Cheng <brady31027@gmail.com>
2016-10-23 23:17:25 -0700

    leetcode 318 v2 (performance tuning)

--- 318.py
+++ 318.py
@@ -4,14 +4,16 @@ class Solution(object):
         :type words: List[str]
         :rtype: int
         """
-        max_prod, long_w, short_w = 0,"",""
-        for w1 in words:
-            for w2 in words:
-                if len(w1) >= len(w2): long_w, short_w = w1, w2
-                if len(list(set(long_w) - set(short_w))) < len(set(long_w)): continue
-                else:
-                    prod = len(long_w) * len(short_w)
-                    if prod > max_prod:
-                        max_prod = prod
+        max_prod, l_nums = 0, []
+        for w in words:
+            w_sum = 0
+            for c in set(w): w_sum |= 1 << (ord(c) - ord('a'))
+            l_nums.append(w_sum)
+        size = len(words)    
+        for w_i in xrange(size):
+            for w_j in xrange(size):
+                if l_nums[w_i] & l_nums[w_j] == 0: 
+                    prod = len(words[w_j]) * len(words[w_i])
+                    if prod > max_prod: max_prod = prod
         return max_prod
-        
+                
