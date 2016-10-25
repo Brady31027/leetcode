5666521c5a42d7c10c66dcdc87dc634cab6aa374
Brady Cheng <brady31027@gmail.com>
2016-10-25 16:41:05 -0700

    leetcode 396 v2

--- 396.py
+++ 396.py
@@ -1,16 +1,14 @@
 class Solution(object):
-    MIN_INT = -(2**31)
     def maxRotateFunction(self, A):
         """
         :type A: List[int]
         :rtype: int
         """
         if len(A) < 2: return 0
-        size = len(A)
-        ans = self.MIN_INT
-        for sft in range(size):
-            tmp_ans = 0
-            for i in range(1, size):
-                tmp_ans += i * A[ (i+sft) % size]
-            if tmp_ans > ans: ans = tmp_ans
+        size, sums = len(A), sum(A)
+        total = sum( i*v for i,v in enumerate(A))
+        ans = total
+        for x in range(size - 1,0,-1):
+            total += sums - size * A[x]
+            ans = max(ans, total)
         return ans
