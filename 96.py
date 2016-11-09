31c396485528078017734ba4d0e1bc5c997f1b4a
Brady Cheng <brady31027@gmail.com>
2016-11-08 16:26:15 -0800

    leetcode 96 v2

--- 96.py
+++ 96.py
@@ -7,7 +7,10 @@ class Solution(object):
         if n == 0: return 1
         elif n == 1: return 1
         elif n == 2: return 2
-        ans = 0
-        for i in xrange(n):
-            ans += self.numTrees(i) * self.numTrees(n-i-1)
-        return ans
+        l_dp = [0 for i in xrange(n+1)]
+        l_dp[0], l_dp[1], l_dp[2] = 1, 1, 2
+        for i in xrange(3, n+1):
+            for j in xrange(1, i+1):
+                l_dp[i] += l_dp[j-1] * l_dp[i-j]
+        return l_dp[n]
+                
