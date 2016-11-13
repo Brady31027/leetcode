29a93786f7662f7d48e26c9c19e4650ebc1c3ca4
Brady Cheng <brady31027@gmail.com>
2016-11-13 00:02:34 -0800

    leetcode 5 v2

--- 5.py
+++ 5.py
@@ -4,16 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        max_len, max_substring = 0, ""
+        max_substring = ""
         
-        def isPalindrome(s):
-            return True if s == s[::-1] else False
+        def getPalindrome(s, start, end):
+            size = len(s)
+            while start >= 0 and end < size and s[start] == s[end]:
+                start, end = start - 1, end + 1
+            return s[start+1:end]
         
-        for i in xrange(0, len(s)):
-            for j in xrange(0, i+1):
-                if isPalindrome(s[j:i+1]) == True:
-                    max_len = max(max_len, i - j + 1)
-                    if max_len == (i - j + 1): max_substring = s[j:i+1]
+        for i in xrange(len(s)):
+            max_substring1 = getPalindrome(s, i, i)
+            max_substring2 = getPalindrome(s, i, i+1)
+            max_substring1 = max_substring1 if len(max_substring1) > len(max_substring2) else max_substring2
+            max_substring = max_substring1 if len(max_substring1) > len(max_substring) else max_substring
         return max_substring
-                
                 
