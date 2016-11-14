7951dfca4e4801366332fa5f73d93cc24e077871
Brady Cheng <brady31027@gmail.com>
2016-11-13 21:48:32 -0800

    leetcode 47 v2 (performance failed)

--- 47.py
+++ 47.py
@@ -4,8 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ll_ans = []
-        for t_permutation in itertools.permutations(nums):
-            l_ans = list(t_permutation)
-            if l_ans not in ll_ans: ll_ans.append(l_ans)
+        size = len(nums)
+        if size == 0: return []
+        elif size == 1: return [nums]
+        else:
+            ll_ans = []
+            for i in xrange(size):
+                for j in self.permuteUnique(nums[:i]+nums[i+1:]):
+                    l_tmp = [nums[i]] + j
+                    if l_tmp in ll_ans: continue
+                    ll_ans.append(l_tmp)
         return ll_ans
