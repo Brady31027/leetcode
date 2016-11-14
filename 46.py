af7362882ed9f849deb410f4004673f7f7d006e8
Brady Cheng <brady31027@gmail.com>
2016-11-13 21:43:32 -0800

    leetcode 46 v2

--- 46.py
+++ 46.py
@@ -1,13 +1,15 @@
-
-from itertools import permutations
 class Solution(object):
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        l_result = []
-        for c in permutations(nums, len(nums)):
-            l_result.append(list(c))
-        return l_result
-        
+        size = len(nums)
+        if size == 0: return []
+        elif size == 1: return [nums]
+        else:
+            ll_ans = []
+            for i in xrange(size):
+                for j in self.permute(nums[:i] + nums[i+1:]):
+                    ll_ans.append([nums[i]]+j)
+        return ll_ans
