ae172b3c532564d61a866100c0eb82945adfd95c
Brady Cheng <brady31027@gmail.com>
2017-01-18 23:09:49 -0800

    two sum go v2

--- 1_Two_Sum/two_sum.go
+++ 1_Two_Sum/two_sum.go
@@ -1,12 +1,10 @@
 func twoSum(nums []int, target int) []int {
     result_map := map[int] int {}
-    i := 0
-    for i < len(nums) {
+    for i:=0; i < len(nums); i++ {
         if val, existed := result_map[target-nums[i]]; existed {
             return []int{val, i}
         }
         result_map[nums[i]] = i
-        i++
     }
     return []int{0,0} // we should never come here!
 }
