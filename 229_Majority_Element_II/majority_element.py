6d2bcb1819987dffc7677bf07e2b79ffb1bdcedb
Brady Cheng <brady31027@gmail.com>
2017-03-12 23:37:40 -0700

    major 2

--- 229_Majority_Element_II/majority_element.py
+++ 229_Majority_Element_II/majority_element.py
@@ -4,13 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        if len(nums) == 0: return []
-        elif len(nums) == 1: return [nums[0]]
-        else:
-            target_ref = len(nums)//3
-            set_nums = set(nums)
-            ans = []
-            for n in set_nums:
-                if nums.count(n) > target_ref:
-                    ans.append(n)
-            return ans
+        major1, major2, count1, count2 = None, None, 0, 0
+        for n in nums:
+            if n == major1: count1 += 1
+            elif n == major2: count2 += 1
+            elif count1 == 0: major1, count1 = n, 1
+            elif count2 == 0: major2, count2 = n, 1
+            else: count1, count2 = count1 - 1, count2 - 1
+        return [ m for m in [major1, major2] if m is not None and nums.count(m) > len(nums)//3]
