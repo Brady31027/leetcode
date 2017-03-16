5beec68d6e4e1980e3ebdccbe39559945f454123
Brady Cheng <brady31027@gmail.com>
2017-03-16 15:37:29 -0700

    update ans

---
+++ 96_Unique_Binary_Search_Trees/unique_binary_search_trees.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0: return 1
+        if n <= 2 : return n
+        dp = [0] * (n+1)
+        dp[0], dp[1], dp[2] = 1, 1, 2
+        for i in range(3, n+1):
+            for j in range(i):
+                dp[i] += (dp[j] * dp[i-j-1])
+        return dp[-1]
