1e4f8b103200188875e9e2685e5b1135fb4e4870
Brady Cheng <brady31027@gmail.com>
2017-03-23 18:49:04 -0700

    add ans

--- 207_Course_Schedule/course_shedule.py
+++ 207_Course_Schedule/course_shedule.py
@@ -4,16 +4,26 @@ class Solution(object):
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: bool
-        """
-        if numCourses < 2 or len(prerequisites) < 2: return True
         
-        while True:
-            remove_cnt = 0
-            nodes = [True] * numCourses
-            for pre in prerequisites: nodes[pre[0]] = False
-            for pre in prerequisites:
-                if nodes[pre[1]] == True:
-                    prerequisites.remove(pre)
-                    remove_cnt += 1
-            if len(prerequisites) == 0: return True
-            elif remove_cnt == 0: return False
+        degrees : for child node, degree stands for the number of parents
+        children : for parent node, children is the list containing child nodes
+        """
+        degrees = [0] * numCourses
+        children = [[] for _ in range(numCourses)]
+        courses = set(range(numCourses))
+        # build up the graph
+        for pre in prerequisites:
+            degrees[ pre[0] ] += 1
+            children[ pre[1] ].append(pre[0])
+        # bfs
+        has_node_being_deleted = True
+        while has_node_being_deleted and len(courses):
+            remove_courses = []
+            has_node_being_deleted = False
+            for x in courses:
+                if degrees[x] == 0: # indep. node
+                    has_node_being_deleted = True
+                    remove_courses.append(x)
+                    for child in children[x]: degrees[child] -= 1
+            for x in remove_courses: courses.remove(x)
+        return len(courses) == 0
