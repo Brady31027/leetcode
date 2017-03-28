866a6eb0cdad64b5f8af8e65afd5152dcdf3c8cf
Brady Cheng <brady31027@gmail.com>
2017-03-28 14:42:42 -0700

    update ans

--- 210_Course_Schedule_II/course_schedule_ii.py
+++ 210_Course_Schedule_II/course_schedule_ii.py
@@ -6,33 +6,29 @@ class Solution(object):
         :rtype: List[int]
         """
         # build up graph
-        unvisited_course, graph = set(), collections.defaultdict(set)
-        for i in range(numCourses): graph[i] = set()
-        for c, p in prerequisites: graph[c].add(p)
-        
-        # find leaf
-        queue1, queue2, queue_ans = [], [], []
-        for course in range(numCourses):
-            if len(graph[course]) == 0: # indep. node
-                queue1.append(course)
-                queue_ans.append(course)
-            unvisited_course.add(course)
+        degree = [0] * numCourses
+        children = [[] for x in range(numCourses)]
+        for c, p in prerequisites:
+            degree[c] += 1
+            children[p].append(c)
+        unvisited_course = set(range(numCourses))
         if not prerequisites: return list(unvisited_course)
+        
         #bfs
         has_node_being_deleted = True
+        queue_ans = []
         while has_node_being_deleted and len(unvisited_course):
             has_node_being_deleted = False
-            for course in queue1:
-                if course in unvisited_course: 
-                    unvisited_course.remove(course)
+            removed_nodes = []
+            for course in unvisited_course:
+                if degree[course] == 0:
+                    for child in children[course]:
+                        degree[child] -= 1
+                    removed_nodes.append(course)
                     has_node_being_deleted = True
-                # update graph
-                for key in graph:
-                    graph[key].discard(course) # remove parent for every node
-                    if key in unvisited_course and key not in queue1 and key not in queue2 and len(graph[key]) == 0:
-                        queue2.append(key)
-            queue_ans += queue2
-            queue1, queue2 = queue2, []
+            for node in removed_nodes:
+                queue_ans.append(node)
+                unvisited_course.remove(node)
         return [] if len(unvisited_course) else queue_ans
             
             
