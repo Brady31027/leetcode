f320c9cc842c3233a0913dcf518fd4e40f75abc6
Brady Cheng <brady31027@gmail.com>
2017-04-03 21:47:58 -0700

    add ans

---
+++ 94_Binary_Tree_Inorder_Traversal/inorder.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        ans, stack = [], []
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                node = stack.pop()
+                ans.append(node.val)
+                root = node.right
+        return ans
