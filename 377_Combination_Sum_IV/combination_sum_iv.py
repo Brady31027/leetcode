34787b322747c58a0d99f46fea95438fb289f914
Brady Cheng <brady31027@gmail.com>
2017-04-06 17:19:11 -0700

    add ans

---
+++ 377_Combination_Sum_IV/combination_sum_iv.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        dp = [1] + [0] * target
+        for i in range(1, target+1):
+            for num in nums:
+                if i >= num: dp[i] += dp[i-num]
+        return dp[-1]
