121dd2af82e2a8afe034ee9b0eca7dc905b15480
Brady Cheng <brady31027@gmail.com>
2017-05-04 10:58:00 -0700

    add ans

---
+++ 371_Sum_of_Two_Integers/sum_of_two_integer.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        # current digit: a ^ b
+        # carry digit: a & b
+        MAX = 0x7FFFFFFF
+        MIN = 0x8FFFFFFF
+        MSK = 0xFFFFFFFF
+        
+        while b != 0:
+            carry = (a & b) & MSK
+            a = (a ^ b) & MSK
+            b = (carry << 1) & MSK
+        return a if a <= MAX else ~(a ^ MSK)
