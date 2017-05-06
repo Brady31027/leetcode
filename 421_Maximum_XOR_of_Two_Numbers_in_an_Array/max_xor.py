c379e538d208be22909b8f7e9d020e778a5084f1
Brady Cheng <brady31027@gmail.com>
2017-05-06 13:25:05 -0700

    update ans

--- 421_Maximum_XOR_of_Two_Numbers_in_an_Array/max_xor.py
+++ 421_Maximum_XOR_of_Two_Numbers_in_an_Array/max_xor.py
@@ -1,12 +1,19 @@
-from itertools import combinations
-
 class Solution(object):
     def findMaximumXOR(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        maxXor = 0
-        for comp in itertools.combinations(nums, 2):
-            maxXor = max(comp[0] ^ comp[1], maxXor)
-        return maxXor
+        ans, mask = 0, 0
+        for bit in range(31, -1, -1):
+            mask = mask | (1 << bit) # 1000, 1100, 1110, 1111
+            candidates = set([ num & mask for num in nums])
+            guess = ans | (1 << bit)
+            for candidate in candidates:
+                # if    a ^ tmp = b
+                # then  a ^  b  = tmp
+                if candidate ^ guess in candidates:
+                    ans = guess
+                    break
+        return ans
+                
