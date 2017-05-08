1489cdde276b8b2cde7dcf89c19fd57e1b266901
Brady Cheng <brady31027@gmail.com>
2017-05-08 00:08:51 -0700

    update ans

--- 344_Reverse_String/Reverse_String.py
+++ 344_Reverse_String/Reverse_String.py
@@ -1,3 +1,9 @@
 class Solution(object):
     def reverseString(self, s):
-        return s[::-1]
+        # return s[::-1]
+        charList = list(s)
+        head, tail = 0, len(charList) - 1
+        while head < tail:
+            charList[head], charList[tail] = charList[tail], charList[head]
+            head, tail = head + 1, tail - 1
+        return "".join(charList)
