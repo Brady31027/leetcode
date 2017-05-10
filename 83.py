2cb60bcc38404e8de887c4c221469f4302c78b22
Brady Cheng <brady31027@gmail.com>
2017-05-09 17:47:16 -0700

    remove redundant file

--- 83.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def deleteDuplicates(self, head):
-        cur = head
-        while cur and cur.next:
-            if cur.next.val == cur.val: cur.next = cur.next.next
-            else: cur = cur.next
-        return head
