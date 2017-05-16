d520f2e78386e4106cf94932c36cbcedb07516c1
Brady Cheng <brady31027@gmail.com>
2017-05-16 16:29:59 -0700

    update ans

--- 187_Repeated_DNA_Sequences/repeated_dna.py
+++ 187_Repeated_DNA_Sequences/repeated_dna.py
@@ -5,12 +5,11 @@ class Solution(object):
         :rtype: List[str]
         """
         size = len(s)
-        memo = collections.defaultdict(int)
+        ans, memo = set(), set()
         for i in xrange( size - 10 + 1):
             pattern = s[i:i + 10]
-            if memo.get(pattern):
-                memo[pattern] += 1
+            if pattern in memo:
+                ans.add(pattern)
             else:
-                memo[pattern] = 1
-        return filter( lambda key: memo[key] > 1, [key for key in memo])
-        
+                memo.add(pattern)
+        return list(ans)
