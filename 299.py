2df61f0e637aa0f7f61a765066b8de8d52e1d368
Brady Cheng <brady31027@gmail.com>
2017-05-16 15:18:23 -0700

    remove redundant file

--- 299.py
+++
@@ -1,6 +0,0 @@
-class sol(object):
-    def guess(self, secret, guess):
-        A, B = sum(map(lambda x, y: x == y, secret, guess)), sum( min(secret.count(x), guess.count(x)) for x in "0123456789") 
-        B = B - A
-        return "%dA%dB" %(A, B)
-
