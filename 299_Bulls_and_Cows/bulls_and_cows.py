07f826592c440c96fba86a0349a31b4fbca86c21
Brady Cheng <brady31027@gmail.com>
2017-05-16 15:15:43 -0700

    add ans

---
+++ 299_Bulls_and_Cows/bulls_and_cows.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getHint(self, secret, guess):
+        """
+        :type secret: str
+        :type guess: str
+        :rtype: str
+        """
+        B = sum((collections.Counter(guess) & collections.Counter(secret)).values())
+        A = sum(itertools.imap( operator.eq, secret, guess))
+        return "%dA%dB"%(A, B-A)
+        
+        # another solution
+        # A, B = sum(map(lambda x, y: x == y, secret, guess)), sum( min(secret.count(x), guess.count(x)) for x in "0123456789")
