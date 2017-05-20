ae7532bda3f6bd687a5fb399e925d707270ca534
Brady Cheng <brady31027@gmail.com>
2017-05-20 15:48:48 -0700

    update ans

--- 554_Brick_Wall/brick_wall.py
+++ 554_Brick_Wall/brick_wall.py
@@ -7,11 +7,7 @@ class Solution(object):
         boundary = collections.Counter()
         for row in xrange(len(wall)):
             accumulatedWidth = 0
-            for col in xrange(len(wall[row]) - 1):
-                accumulatedWidth += wall[row][col]
-                boundary[accumulatedWidth] += 1
-        verticalLineCandidates = filter( lambda (k, v): k > 0 , [ (k, v) for k, v in boundary.iteritems()] )
-        verticalLineRepeatCount = 0
-        if len(verticalLineCandidates) > 0:
-            verticalLineRepeatCount = max(verticalLineCandidates, key=lambda tup: tup[1])[1]
-        return len(wall) - verticalLineRepeatCount
+            for brick in wall[row]:
+                if accumulatedWidth > 0: boundary[accumulatedWidth] += 1
+                accumulatedWidth += brick # skip the last brick
+        return len(wall) - (max(boundary.values() or [0]))
