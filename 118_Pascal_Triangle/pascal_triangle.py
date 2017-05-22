8426dd7720c46843467b54e92a2df7b13b419837
Brady Cheng <brady31027@gmail.com>
2017-05-21 21:00:05 -0700

    add ans

---
+++ 118_Pascal_Triangle/pascal_triangle.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        # e.g [1, 1] to [1, 2, 1]
+        # extend [1, 1] to [0, 1, 1] and [1, 1, 0]
+        # sum up [0, 1, 1] and [1, 1, 0] to form [1, 2, 1]
+        ans = [[1]]
+        for i in xrange(1, numRows):
+            ans += [map(lambda x, y: x + y, [0] + ans[-1], ans[-1] + [0])]
+        return ans
+        
