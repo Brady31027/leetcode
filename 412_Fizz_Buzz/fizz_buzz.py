91a9e532732f7b448935ba68f7ea17f4e969b2a0
Brady Cheng <brady31027@gmail.com>
2017-05-22 00:46:05 -0700

    update ans

--- 412_Fizz_Buzz/fizz_buzz.py
+++ 412_Fizz_Buzz/fizz_buzz.py
@@ -4,10 +4,17 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        ans = []
-        for num in xrange(1, n + 1):
-            if num % 15 == 0: ans.append("FizzBuzz")
-            elif num % 5 == 0: ans.append("Buzz")
-            elif num % 3 == 0: ans.append("Fizz")
-            else: ans.append(str(num))
+        ans = [ str(i) for i in xrange(1, n + 1) ]
+        fizz, buzz, fizzbuzz = 3, 5, 15
+        while fizz <= n or buzz <= n:
+            if fizz <= n:
+                ans[fizz - 1] = "Fizz"
+                fizz += 3
+            if buzz <= n:
+                ans[buzz - 1] = "Buzz"
+                buzz += 5
+        while fizzbuzz <= n:
+            if fizzbuzz <= n:
+                ans[fizzbuzz - 1] = "FizzBuzz"
+                fizzbuzz += 15
         return ans
