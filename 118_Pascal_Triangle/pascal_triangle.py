e1d741b2f6b687ab40ecadabebb1014df2cd3ea3
Brady Cheng <brady31027@gmail.com>
2017-05-21 21:01:00 -0700

    update ans

--- 118_Pascal_Triangle/pascal_triangle.py
+++ 118_Pascal_Triangle/pascal_triangle.py
@@ -7,6 +7,7 @@ class Solution(object):
         # e.g [1, 1] to [1, 2, 1]
         # extend [1, 1] to [0, 1, 1] and [1, 1, 0]
         # sum up [0, 1, 1] and [1, 1, 0] to form [1, 2, 1]
+        if numRows == 0: return []
         ans = [[1]]
         for i in xrange(1, numRows):
             ans += [map(lambda x, y: x + y, [0] + ans[-1], ans[-1] + [0])]
