25173cdadd6c3e28fc99cff76a7393f4a439178f
Brady Cheng <brady31027@gmail.com>
2017-05-24 12:31:26 -0700

    update ans

--- 31_Next_Permutation/next_permutation.py
+++ 31_Next_Permutation/next_permutation.py
@@ -11,12 +11,12 @@ class Solution(object):
             if nums[i] > nums[i - 1]:
                 anchor = i - 1 # 0
                 break
-        if anchor + 1 > 0:
+        if anchor >= 0:
             for i in xrange(size - 1, -1, -1):
                 if nums[i] > nums[anchor]:
                     nums[i], nums[anchor] = nums[anchor], nums[i]
                     break
-        nums[anchor + 1:] = reversed(nums[anchor + 1:])
+        nums[anchor + 1:] = sorted(nums[anchor + 1:])
             
                 
                 
