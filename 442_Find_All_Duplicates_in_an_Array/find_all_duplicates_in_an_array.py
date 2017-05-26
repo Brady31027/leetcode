0bd7367ec907c262ad4c300f0e48a0e0742189b1
Brady Cheng <brady31027@gmail.com>
2017-05-25 23:24:54 -0700

    add ans

---
+++ 442_Find_All_Duplicates_in_an_Array/find_all_duplicates_in_an_array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for n in nums:
+            if nums[ abs(n) - 1 ] < 0:
+                ans.append(abs(n))
+            else:
+                nums[ abs(n) - 1] = -nums[ abs(n) - 1]
+        return ans
