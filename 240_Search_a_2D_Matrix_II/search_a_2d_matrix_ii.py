e748e790ebba9be2fcfec9d352b7e91c0527a2e2
Brady Cheng <brady31027@gmail.com>
2017-05-28 00:35:02 -0700

    add ans

---
+++ 240_Search_a_2D_Matrix_II/search_a_2d_matrix_ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+        row, col = 0, len(matrix[0]) - 1
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+        return False
