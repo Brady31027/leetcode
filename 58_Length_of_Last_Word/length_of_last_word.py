000941bab96d80ee80e4f9b39c42774b56fcc85e
Brady Cheng <brady31027@gmail.com>
2017-05-29 00:00:50 -0700

    update ans

--- 58_Length_of_Last_Word/length_of_last_word.py
+++ 58_Length_of_Last_Word/length_of_last_word.py
@@ -4,5 +4,4 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # remember to remove the tailing spaces
-        return len((re.sub(r'\s+$','',s)).split(' ')[-1]) if s else 0
+        return len(s.strip().split(' ')[-1])
