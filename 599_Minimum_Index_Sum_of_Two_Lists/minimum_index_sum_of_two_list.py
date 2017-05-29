c07d07e99fa077e2a6741f80ca747687a3a7c987
Brady Cheng <brady31027@gmail.com>
2017-05-29 16:59:27 -0700

    update ans

--- 599_Minimum_Index_Sum_of_Two_Lists/minimum_index_sum_of_two_list.py
+++ 599_Minimum_Index_Sum_of_Two_Lists/minimum_index_sum_of_two_list.py
@@ -5,10 +5,13 @@ class Solution(object):
         :type list2: List[str]
         :rtype: List[str]
         """
-        ans, common = collections.defaultdict(list), set(list1).intersection(set(list2))
-        for k1, v1 in enumerate(list1):
-            if v1 not in common: continue
-            for k2, v2 in enumerate(list2):
-                if v2 not in common or v2 != v1: continue
-                ans[ k1 + k2 ].append(v1)
-        return ans[min(ans)]
+        tbl1 = {v: k for k, v in enumerate(list1) }
+        minimal, ans = 1e9, []
+        for k, v in enumerate(list2):
+            i = tbl1.get(v, 1e9)
+            if i + k < minimal:
+                minimal = i + k
+                ans = [v]
+            elif i + k == minimal:
+                ans.append(v)
+        return ans
