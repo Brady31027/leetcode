2de1709d94a2b87dc74e874bbafcf02cc4e9f4dd
Brady Cheng <brady31027@gmail.com>
2017-05-29 00:02:51 -0700

    remove redundant file

--- 58.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        return 0 if len(s) == 0 else 0 if len(s.split()) == 0  else len(s.split()[-1])
-        
