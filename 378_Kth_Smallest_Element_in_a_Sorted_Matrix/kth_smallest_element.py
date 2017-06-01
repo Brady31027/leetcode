971bdfa83aaff35f8a8a16fc19a0af0d4947db3d
Brady Cheng <brady31027@gmail.com>
2017-06-01 01:04:11 -0700

    add ans

--- 378_Kth_Smallest_Element_in_a_Sorted_Matrix/kth_smallest_element.py
+++ 378_Kth_Smallest_Element_in_a_Sorted_Matrix/kth_smallest_element.py
@@ -5,22 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        rowLen, colLen = len(matrix), len(matrix[0])
-        visitedMap = set()
-        
-        def expend(y, x):
-            if y + 1 < rowLen and (y + 1, x) not in visitedMap:
-                heapq.heappush(heap, [matrix[y + 1][x], y + 1, x])
-                visitedMap.add((y + 1, x))
-            if x + 1 < colLen and (y, x + 1) not in visitedMap:
-                heapq.heappush(heap, [matrix[y][x + 1], y, x + 1])
-                visitedMap.add((y, x + 1))
+        rowLen, colLen, val = len(matrix), len(matrix[0]), matrix[0][0]
+        visitedMap = [[False for _ in xrange(colLen)] for _ in xrange(rowLen)]
         heap = [[matrix[0][0], 0, 0]]
-        heapq.heapify(heap)
         
-        val = matrix[0][0]
         for _ in xrange(k):
             val, y, x = heapq.heappop(heap)
-            expend(y, x)
-            
+            if y + 1 < rowLen and visitedMap[y + 1][x] == False:
+                heapq.heappush(heap, [matrix[y + 1][x], y + 1, x])
+                visitedMap[y + 1][x] = True
+            if x + 1 < colLen and visitedMap[y][x + 1] == False:
+                heapq.heappush(heap, [matrix[y][x + 1], y, x + 1])
+                visitedMap[y][x + 1] = True
+        
         return val
