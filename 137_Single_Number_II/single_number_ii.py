981368fbd8b3d90a5338a4388a36feb0a84faa8f
Brady Cheng <brady31027@gmail.com>
2017-08-07 11:43:42 -0700

    update python

--- 137_Single_Number_II/single_number_ii.py
+++ 137_Single_Number_II/single_number_ii.py
@@ -4,18 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # early reject for [x] or [x, x]
-        if len(nums) < 3: return nums[0]
-        
-        nums.sort()
-        prevNumber, repeatCount = None, 0
-        for n in nums:
-            if n != prevNumber:
-                if repeatCount < 3 and prevNumber is not None:
-                    return prevNumber
-                # reset counter
-                repeatCount = 1
-            else:
-                repeatCount += 1
-            prevNumber = n
-        return nums[-1]
+        return ( sum(set(nums)) * 3 - sum(nums) ) / 2
+        """
+        ans = 0
+        for i in xrange(32):
+            sum = 0
+            for n in nums:
+                if n >> i & 1:
+                    sum += 1
+            sum %= 3
+            ans |= (sum << i)
+        if ans > 0x7FFFFFFF:
+            return -int(0x100000000 - ans)
+        return ans
+        """
