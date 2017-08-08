e458ccb8b1dea31cb4f19b4d183332fe294535f3
Brady Cheng <brady31027@gmail.com>
2017-08-08 11:39:39 -0700

    add ans

--- 507_Perfect_Number/perfect_num.py
+++ 507_Perfect_Number/perfect_num.py
@@ -4,20 +4,14 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        head, tail = 2, int(num/2 + 0.5)
-        total = 1
-        while head <= tail:
+        if num < 2: return False
+        head, total = 2, 1
+        while head * head <= num:
             if num % head == 0:
-                tail = num // head
-                
-                if tail < head: break
-                
-                if head == tail: total += head
-                else: total += head + tail
-                
+                tail = num / head    
+                if head != tail: total += head + tail
+                else: total += head
                 if total > num: return False
             head += 1
-        return True if total == num else False
-            
-        
+        return total == num
         
