44979222a0647edb9eb40dff3a567c0ce67ac799
Brady Cheng <brady31027@gmail.com>
2017-08-09 01:14:11 -0700

    add ans

---
+++ 521_Longest_Uncommon_Subsequence_I/longest_uncommon_subsequence.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        return max(len(a), len(b)) if a != b else -1
+        
